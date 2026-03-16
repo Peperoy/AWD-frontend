66662c4a694675912abb5206f34c1fb95c83ca08
Peperoy <deboeverhenri@gmail.com>
2026-03-16 19:09:39 +0100

    vercel analytic

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import Head from 'next/head';
-import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import GoogleAnalytics from '../components/GoogleAnalytics';
 import CookieBanner from '../components/CookieBanner';
 
